acc48b6110a6fdc27285599abb104c89f0e41946
Philip Delff <philip@delff.dk>
2023-05-23 07:41:05 -0400

    more tests

--- tests/testthat/test_NMcreateDoses.R
+++ tests/testthat/test_NMcreateDoses.R
@@ -48,45 +48,69 @@ test_that("II/ADDL",{
 ### covariates
 test_that("covariates basics",{
     fileRef <- "testReference/NMcreateDoses_04.rds"
+
+    res <- NMcreateDoses(TIME=c(0),AMT=data.table(DOSE=c(10,50),AMT=c(10,50)))
+    expect_equal_to_reference(res,fileRef)
+})
+
+
+test_that("covariates basics 2",{
+    fileRef <- "testReference/NMcreateDoses_05.rds"
     res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1)
 
     expect_equal_to_reference(res,fileRef)
 })
 
 
-### covariates not spanning same covariate values. Should not be supported for now.
-res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=list(II=12,ADDL=3,regimen="MD"))
-res
+### TODO: covariates not spanning same covariate values. Should not be supported for now.
+test_that("covariates not spanning same covariate values",{
+    fileRef <- "testReference/NMcreateDoses_06.rds"
 
-## covariates not spanning same covariate values. Should not be supported for now.
-res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=12,ADDL=3,regimen="MD"))
-res
-
-## This looks OK.
-NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=c(0,0,0,12),ADDL=c(0,0,0,3),regimen=c("SD",rep("MD",3))))
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=list(II=12,ADDL=3,regimen="MD"))
+    ##res
+    expect_equal_to_reference(res,fileRef)
+})
 
 
-NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=data.table(AMT=c(10,5),regimen=c("SD","MD")),CMT=1)
+
+test_that("covariates spanning same covariate values",{
+    ## This looks OK.
+    fileRef <- "testReference/NMcreateDoses_07.rds"
+
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=data.table(AMT=c(10,5),regimen=c("SD","MD")),CMT=1)
+    expect_equal_to_reference(res,fileRef)
+})
 
 
-NMcreateDoses(TIME=c(0),AMT=data.table(DOSE=c(10,50),AMT=c(10,50)))
+test_that("covariates spanning same covariate values - addl",{
+    ## This looks OK.
+    fileRef <- "testReference/NMcreateDoses_08.rds"
 
-## ID as covariate - not supported
-expect_error(
-    NMcreateDoses(TIME=c(0),AMT=data.table(ID=c(10,50),AMT=c(10,50)))
-)
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=c(0,0,0,12),ADDL=c(0,0,0,3),regimen=c("SD",rep("MD",3))))
+    expect_equal_to_reference(res,fileRef)
+})
+
+
+
+test_that("ID as covariate - not supported",{
+    expect_error(
+        NMcreateDoses(TIME=c(0),AMT=data.table(ID=c(10,50),AMT=c(10,50)))
+    )
+}
 
 
 ## not sure what extra this example is showing
 ## NMcreateDoses(TIME=data.table(ID=c(1,1,2,2,2),TIME=c(0,1,0,1,4)),AMT=data.table(ID=c(1,1,2,2),AMT=c(2,1,4,2)))
 
-## not ok - stop necessary
-expect_error(
-    NMcreateDoses(TIME=c(0,1,4),AMT=data.table(AMT=c(2,1,4,2)))
-)
 
-## ok
-NMcreateDoses(TIME=c(0,1,4),AMT=data.table(DOSELEVEL=c(1,1,2,2),AMT=c(2,1,4,2)))
+
+test_that("Expanding AMT within a covariate",{
+    fileRef <- "testReference/NMcreateDoses_09.rds"
+
+    res <- NMcreateDoses(TIME=c(0,1,4),AMT=data.table(DOSELEVEL=c(1,1,2,2),AMT=c(2,1,4,2)))
+    expect_equal_to_reference(res,fileRef)
+})
+
 
 ## must return error. AMT is longer than TIME.
 ### this is a bug. Difference because amt is not unique.
@@ -101,7 +125,7 @@ NMcreateDoses(TIME=c(0),AMT=c(2,1),CMT=2)
 NMcreateDoses(TIME=0,AMT=10)
 NMcreateDoses(TIME=0,AMT=10,as.fun=tibble::as_tibble)
 
-}
+
 
 
 
