b2cfd95f1c2da32ff4282019f5d81c7d9a9f689a
Philip Delff <philip@delff.dk>
2023-05-23 07:21:28 -0400

    tests

--- tests/testthat/test_NMcreateDoses.R
+++ tests/testthat/test_NMcreateDoses.R
@@ -27,64 +27,70 @@ test_that("Expand columns",{
 
     expect_equal_to_reference(res,fileRef)
 
-}
+})
 
 test_that("makes no sense to expand TIME",{
-    expect_error(NMcreateDoses(TIME=c(0),AMT=c(0,10),CMT=2))
-}
+    expect_error(
+        NMcreateDoses(TIME=c(0),AMT=c(0,10),CMT=2)
+    )
+})
 
-## II/ADDL should only be applied to last event. addl.lastonly
-## argument?
-NMcreateDoses(TIME=c(0,12),AMT=10,addl=list(II=12,ADDL=3),CMT=2)
+
+if(F) { ## ready, but must be implemented in testthat
+
+    ## II/ADDL should only be applied to last event. addl.lastonly
+    ## argument?
+    NMcreateDoses(TIME=c(0,12),AMT=10,addl=list(II=12,ADDL=3),CMT=2)
 
 
 ### covariates
-res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1)
-res
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1)
+    res
 
 
 ### covariates not spanning same covariate values. Should not be supported for now.
-res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=list(II=12,ADDL=3,regimen="MD"))
-res
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=list(II=12,ADDL=3,regimen="MD"))
+    res
 
-## covariates not spanning same covariate values. Should not be supported for now.
-res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=12,ADDL=3,regimen="MD"))
-res
+    ## covariates not spanning same covariate values. Should not be supported for now.
+    res <- NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=12,ADDL=3,regimen="MD"))
+    res
 
-## This looks OK.
-NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=c(0,0,0,12),ADDL=c(0,0,0,3),regimen=c("SD",rep("MD",3))))
-
-
-NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=data.table(AMT=c(10,5),regimen=c("SD","MD")),CMT=1)
+    ## This looks OK.
+    NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=10,CMT=1,addl=data.table(II=c(0,0,0,12),ADDL=c(0,0,0,3),regimen=c("SD",rep("MD",3))))
 
 
-NMcreateDoses(TIME=c(0),AMT=data.table(DOSE=c(10,50),AMT=c(10,50)))
+    NMcreateDoses(TIME=data.table(regimen=c("SD","MD","MD"),TIME=c(0,0,12)),AMT=data.table(AMT=c(10,5),regimen=c("SD","MD")),CMT=1)
 
-## ID as covariate - not supported
-expect_error(NMcreateDoses(TIME=c(0),AMT=data.table(ID=c(10,50),AMT=c(10,50))))
 
-## not sure what extra this example is showing
-## NMcreateDoses(TIME=data.table(ID=c(1,1,2,2,2),TIME=c(0,1,0,1,4)),AMT=data.table(ID=c(1,1,2,2),AMT=c(2,1,4,2)))
+    NMcreateDoses(TIME=c(0),AMT=data.table(DOSE=c(10,50),AMT=c(10,50)))
 
-## not ok - stop necessary
-expect_error(NMcreateDoses(TIME=c(0,1,4),AMT=data.table(AMT=c(2,1,4,2))))
+    ## ID as covariate - not supported
+    expect_error(NMcreateDoses(TIME=c(0),AMT=data.table(ID=c(10,50),AMT=c(10,50))))
 
-## ok
-NMcreateDoses(TIME=c(0,1,4),AMT=data.table(DOSELEVEL=c(1,1,2,2),AMT=c(2,1,4,2)))
+    ## not sure what extra this example is showing
+    ## NMcreateDoses(TIME=data.table(ID=c(1,1,2,2,2),TIME=c(0,1,0,1,4)),AMT=data.table(ID=c(1,1,2,2),AMT=c(2,1,4,2)))
 
-## must return error. AMT is longer than TIME.
+    ## not ok - stop necessary
+    expect_error(NMcreateDoses(TIME=c(0,1,4),AMT=data.table(AMT=c(2,1,4,2))))
+
+    ## ok
+    NMcreateDoses(TIME=c(0,1,4),AMT=data.table(DOSELEVEL=c(1,1,2,2),AMT=c(2,1,4,2)))
+
+    ## must return error. AMT is longer than TIME.
 ### this is a bug. Difference because amt is not unique.
-NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,2))
-NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,20))
-## must return error. AMT is longer than TIME.
-NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,2),CMT=2)
-NMcreateDoses(TIME=c(0),AMT=c(2,1),CMT=2)
+    NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,2))
+    NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,20))
+    ## must return error. AMT is longer than TIME.
+    NMcreateDoses(TIME=c(0,1,4),AMT=c(2,1,4,2),CMT=2)
+    NMcreateDoses(TIME=c(0),AMT=c(2,1),CMT=2)
 
 
-## as.fun
-NMcreateDoses(TIME=0,AMT=10)
-NMcreateDoses(TIME=0,AMT=10,as.fun=tibble::as_tibble)
+    ## as.fun
+    NMcreateDoses(TIME=0,AMT=10)
+    NMcreateDoses(TIME=0,AMT=10,as.fun=tibble::as_tibble)
 
+}
 
 
 
