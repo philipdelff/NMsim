5b6e3460f4e7e8aaa627c5dc60ef44a7eee81da1
Philip Delff <philip@delff.dk>
2024-01-29 07:26:15 -0500

    windows tests

--- devel/NMsim_test_win.R
+++ devel/NMsim_test_win.R
@@ -12,21 +12,19 @@ dt.sim[,BBW:=40][,ROW:=.I]
 dt.sim.known <- egdt(dt.sim[,!("ID")],data.table(ID=101:105))
 setorder(dt.sim.known,ID,TIME,EVID,CMT)
 
-
+#### We want NMsim to run without PSN. Tell NMsim where to find the
+#### Nonmem executable.
 ## NMdataConf(dir.psn="/opt/psn")
 NMdataConf(path.nonmem ="C:/nm75g64/run/nmfe75.bat")
 NMdataConf(dir.psn=NULL)
-##
 
-## 025 doesn't seem stable. Got Q~1e7 and Nonmem didn't run
+
+
 file.mod <- "../tests/testthat/testData/nonmem/xgxr021.mod"
-## NMdata:::NMreadExt( fnExtension(file.mod,"ext"))
-## library(nonmem2R)
-## extload(file.mod)
 
-load_all()
 
 set.seed(43)
+## Not specifying method.execute - PSN should not be used if NMdataConf was used to specify Nonmem path. 
 simres <- NMsim(file.mod,
                 data=dt.sim,
                 text.table="PRED IPRED",
@@ -35,11 +33,35 @@ simres <- NMsim(file.mod,
                 ,method.update.inits="nmsim"
                 )
 
+## use PSN (path to folder containing "execute" executable may need to be specified above.
+simres <- NMsim(file.mod,
+                data=dt.sim,
+                text.table="PRED IPRED",
+                dir.sims="testOutput",
+                name.sim="default_01"
+                ,method.update.inits="nmsim"
+               ,method.execute="psn"
+                )
 
-## not specifying method.update.inits
+## Explicitly ask to use Nonmem directly
+simres <- NMsim(file.mod,
+                data=dt.sim,
+                text.table="PRED IPRED",
+                dir.sims="testOutput",
+                name.sim="default_01"
+                ,method.update.inits="nmsim"
+               ,method.execute="NMsim"
+                )
+
+## not specifying method.update.inits - default is using PSN if found
 simres <- NMsim(file.mod,
                 data=dt.sim,
                 text.table="PRED IPRED",
                 dir.sims="testOutput",
                 name.sim="default_01"
                 )
+
+### next step is to test an example with method.sim=NMsim_known
+
+
+
