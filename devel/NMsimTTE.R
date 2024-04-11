1e5e5409bd90a0f9efaa8b721fc0541056d619c6
Philip Delff <philip@delff.dk>
2024-04-11 14:04:51 -0400

    adding test data

---
+++ devel/NMsimTTE.R
@@ -0,0 +1,10 @@
+## number of TTE endpoints
+
+## number of comp in .mod
+
+## extend or create COM()
+
+## add differential equations for each endpoint
+
+## Based on surv variable, sim TTE endpoints
+
