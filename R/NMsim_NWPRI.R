8fa5205d54f6751479c8e91bb640e8ca889c6d6d
Philip Delff <philip@delff.dk>
2024-08-23 09:45:19 -0400

    passing checks

--- R/NMsim_NWPRI.R
+++ R/NMsim_NWPRI.R
@@ -14,7 +14,7 @@
 ##'     and SIGMA are simulated from the inverse-Wishart
 ##'     distribution. Correlations of OMEGA and SIGMA parameters will
 ##'     only be applied within modeled "blocks".
-##' @references \href{https://ascpt.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1002%2Fpsp4.12422&file=psp412422-sup-0001-Supinfo1.pdf}{inverse-Wishart degrees of freedom calculation for OMEGA and SIGMA: NONMEM tutorial part II, supplement 1, part C.}
+##' @references \href{https://ascpt.onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1002\%2Fpsp4.12422&file=psp412422-sup-0001-Supinfo1.pdf}{inverse-Wishart degrees of freedom calculation for OMEGA and SIGMA: NONMEM tutorial part II, supplement 1, part C.}
 ##' @seealso NMsim_VarCov
 ##' @import NMdata
 ##' @import data.table
