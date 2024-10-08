5fcc8ea9c814beb40a0fc528b61c2f72503ba043
Philip Delff <philip@delff.dk>
2024-10-08 13:06:10 -0400

    export add and modify

--- R/modifyHelpers.R
+++ R/modifyHelpers.R
@@ -1,5 +1,13 @@
-#### Do not export these functions. They should just be available to define arguments in `modify.model`
-
+##' Create function that adds text elements to vector
+##' @param ... Elements to add.
+##' @param .pos Either \"top\" or \"bottom\". Decides if new text is prepended or appended to existing text.
+##' @return A function that adds the specified text to character vectors
+##' @examples
+##' myfun <- add("b","d")
+##' myfun("a")
+##' myfun2 <- add("b","d",.pos="top")
+##' myfun2("a")
+##' @export
 add <- function(...,.pos="bottom"){
     
     switch(.pos,
@@ -9,6 +17,16 @@ add <- function(...,.pos="bottom"){
     
 }
 
+##' Create function that modifies text elements in a vector
+##' @param ... Passed to `gsub()`
+##' @return A function that runs `gsub` to character vectors
+##' @examples
+##' myfun <- overwrite("b","d")
+##' myfun(c("a","b","c","abc"))
+##' ## regular expressions
+##' myfun2 <- overwrite("b.*","d")
+##' myfun2(c("a","b","c","abc"))
+##' @export
 overwrite <- function(...){
 
     function(x){
