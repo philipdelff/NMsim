0c6034f1fe1015f93e5e8e61dd77ff9edef97449
Philip Delff <philip@delff.dk>
2024-10-09 09:20:01 -0400

    cleaning

--- devel/compare_models/033_vs_043_S2.R
+++ devel/compare_models/033_vs_043_S2.R
@@ -5,15 +5,33 @@ load_all("~/wdirs/NMdata")
 NMdataConf(path.nonmem="/opt/NONMEM/nm75/run/nmfe75"
            ,as.fun="data.table")
 
-files <- c("../nonmem/xgxr033.mod","../nonmem/xgxr043.mod")
+files <- c("../nonmem/xgxr033.mod"
+          ,"../nonmem/xgxr043.mod"
+          ## ,"../nonmem/xgxr044.mod"
+          ## ,"../nonmem/xgxr045.mod"
+          ,"../nonmem/xgxr046.mod"
+           )
+
 exts <- NMreadExt(file=files)
 
 dcast(exts,parameter~model,value.var="value")
+NMreadExt(file=files,return="OBJ")
+
 
 res <- NMscanMultiple(files=files)
 
 ### Predictions are identical
 load_all("~/wdirs/NMgof")
-NMplotIndProfs(res,par.prof="model")
+NMplotIndProfs(res[ID>170],par.prof="model")
 
 dcast(res[ID==180],TIME~model,value.var="IPRED")
+
+
+##
+pars <- findCovs(res,by="model")
+cnames <- colnames(pars)
+pnames <- cnames[grepl("^LTV",cnames)]
+p.l <- melt(pars,measure.vars=c(pnames),id.vars="model")
+p.l[,tvval.lin:=exp(value)]
+p.l[,par.lin:=sub("LTV","",variable)]
+dcast(p.l,par.lin~model,value.var="tvval.lin")
--- inst/examples/R/run_models.R
+++ inst/examples/R/run_models.R
@@ -7,8 +7,11 @@ NMdataConf(as.fun="data.table")
 
 NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr022.mod",sge=FALSE)
 NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr032.mod",sge=FALSE)
-NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr033.mod",sge=FALSE)
-NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr043.mod",sge=FALSE)
+NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr033.mod",sge=TRUE,method.execute="psn",nc=72)
+NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr043.mod",sge=TRUE,method.execute="psn",nc=72)
+NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr044.mod",sge=TRUE,method.execute="psn",nc=72)
+NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr045.mod",sge=TRUE,method.execute="psn",nc=72)
+NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr046.mod",sge=FALSE)
 NMexec("~/wdirs/NMsim/inst/examples/nonmem/xgxr132.mod",sge=FALSE)
 
 
